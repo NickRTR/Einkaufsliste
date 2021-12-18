3d6f89d09c81ee321686ecc4ab35a1dd1f96b8ec
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-18 23:04:48 +0100

    localStorage error

--- src/data/ProductData.js
+++ src/data/ProductData.js
@@ -1,6 +1,24 @@
 import {writable} from "svelte/store";
+import {browser} from "$app/env";
 
-const ProductList = writable(JSON.parse(localStorage.getItem("ProductList")) || [
+if (browser) {
+    const ProductList = writable(JSON.parse(localStorage.getItem("ProductList")) || [
+        {
+            id: 1,
+            title: "Bier",
+            checked: false,  
+        },
+        {
+            id: 0,
+            title: "Apfel",
+            checked: false,  
+        },
+    ]);
+
+    ProductList.subscribe((value) => localStorage.ProductList = JSON.stringify(value));
+}
+
+const ProductList = writable([
     {
         id: 1,
         title: "Bier",
@@ -11,8 +29,6 @@ const ProductList = writable(JSON.parse(localStorage.getItem("ProductList")) || 
         title: "Apfel",
         checked: false,  
     },
-])
-
-ProductList.subscribe((value) => localStorage.ProductList = JSON.stringify(value));
+]);
 
 export default ProductList;
