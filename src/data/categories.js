ce7340002bdada69768e82d3124703a9eb072662
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-22 19:25:05 +0100

    more categories, new check system

---
+++ src/data/categories.js
@@ -0,0 +1,42 @@
+// food categories
+
+export const categories = {
+    // Milch
+    milk: ["milch", "butter", "sahne", "joghurt", "käse", "Frischkäse"],
+
+    // Wurst, Fleisch
+    sausage: ["fleisch", "wurst"],
+
+    // Gefrorenes
+    frozen: ["pommes", "gefroren"],
+
+    // Früchte, Obst
+    fruit: ["obst", "früchte", "apfel", "äpfel", "kirsche", "melone"],
+
+    // Gemüse
+    vegetables: ["paprika", "zwiebel", "kartoffel", "gurke", "salat"],
+
+    // Dosenartikel
+    canned: ["ravioli", "dosenwurst", "bohnen"],
+
+    // Getränke
+    drink: ["bier", "wasser", "saft", "orangensaft", "alkohol"],
+
+    // Drogierieartikel
+    drogery: ["waschmittel", "spülmittel", "zahnpasta", "duschgel"],
+
+    // Nudeln
+    noodles: ["nudeln", "spaghetti"],
+
+    // Süßigkeiten
+    sweets: ["nimm2", "haribo", "gummibärchen", "schokolade"],
+
+    // Brot, Gebäck
+    bread: ["brot", "brötchen", "wecken", "gebäck"],
+
+    // Soßen
+    sauce: ["soße", "bratensoße", "ketchup"],
+
+    // Brotaufstrich
+    jam: ["marmelade", "erdbeermarmelade", "kirschmarmelade", "nutella"],
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
+    import {categories} from "../data/categories.js";
+
     // firebase
     import {initializeApp, getApps, getApp} from "firebase/app";
     import {getFirestore, collection, onSnapshot, doc, updateDoc, deleteDoc, addDoc, query, orderBy, getDocs, Timestamp} from "firebase/firestore";
-    import {firebaseConfig} from "../data/firebaseConfig.js"
-    import {browser} from "$app/env"
+    import {firebaseConfig} from "../data/firebaseConfig.js";
+    import {browser} from "$app/env";
 
     const firebaseApp = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
     const db = browser && getFirestore();
@@ -35,57 +37,120 @@
         }
     }
 
-    // delete
+    // delete Product
     const deleteProduct = async (id) => {
         await deleteDoc(doc(db, "products", id));
-        
     }
 
     // toggle checked
-    const toggleChecked = async (id, status) => {
+    const toggleChecked = async (id, created, status) => {
+        let time = created;
+        if (status == true) {
+            time = Timestamp.now();
+        }
         await updateDoc(doc(db, "products", id), {
             checked: !status,
+            created: time,
         });
     };
 
     // add Products
-    $: input = ""
+    $: input = "";
 
     const handleInput = async () => {
         if (input !== "") {
             await addDoc(collection(db, "products"), {
                 title: input,
                 checked: false,
-                category: input,
+                category: getCategory(input),
                 created: Timestamp.now(),
             });
         }
         input = "";
     };
+
+    function getCategory(input) {
+        // choose category corresponding to categories array
+
+        if (categories.milk.includes(input.toLowerCase())) {
+            return "milk";
+        }
+        else if (categories.sausage.includes(input.toLowerCase())) {
+            return "sausage";
+        }
+        else if (categories.frozen.includes(input.toLowerCase())) {
+            return "frozen";
+        }
+        else if (categories.fruit.includes(input.toLowerCase())) {
+            return "fruit";
+        }
+        else if (categories.vegetables.includes(input.toLowerCase())) {
+            return "vegetables";
+        }
+        else if (categories.canned.includes(input.toLowerCase())) {
+            return "canned";
+        }
+        else if (categories.drink.includes(input.toLowerCase())) {
+            return "drink";
+        }
+        else if (categories.drogery.includes(input.toLowerCase())) {
+            return "drogery";
+        }
+        else if (categories.noodles.includes(input.toLowerCase())) {
+            return "noodles";
+        }
+        else if (categories.sweets.includes(input.toLowerCase())) {
+            return "sweet";
+        }
+        else if (categories.bread.includes(input.toLowerCase())) {
+            return "bread";
+        }
+        else if (categories.sauce.includes(input.toLowerCase())) {
+            return "sauce";
+        }
+        else if (categories.jam.includes(input.toLowerCase())) {
+            return "jam";
+        }
+        else {
+            return "choose"
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Einkaufsliste</title>
 </svelte:head>
 
-<main class="text-center text-bee bg-marine l min-h-screen h-full pb-1">
-    <h1 class="text-4xl py-5 font-semibold">Einkaufsliste</h1>
-    <form class="flex mb-4 mt-1 justify-center" on:submit|preventDefault={handleInput}>
-        <input class="m-0 w-3/4 h-8 px-2 bg-bee border-none text-lg text-marine font-semibold rounded-xl" type="text" bind:value={input}>
-        <button class="text-lg font-semibold px-2 ml-1.5 bg-bee text-marine rounded-xl" type="submit">Add</button>
-    </form>
-    <div class="products bg-marine">
-        {#each products as product}
-            <div class="Card text-2xl text-left p-1.5 m-2.5 bg-marine-bright rounded-xl break-words flex items-center">
-                    <input class="h-10 w-10 mr-1.5" type="image" src="/category/{product.category}.svg" alt="(delete)" on:click={() => {deleteProduct(product.id)}}>
-                    <p class="" on:click={() => {toggleChecked(product.id, product.checked)}}>{product.title}</p>
-                    <input class="ml-auto mr-1 w-6 h-6" type="checkbox" checked={product.checked} on:click={() => {toggleChecked(product.id, product.checked)}}>
-                <!-- <div class="text-2xl col-span-11 mb-1">
-                    <input class="align-middle w-5 h-5" type="checkbox" id="checkbox+{product.id}" checked={product.checked} on:click={() => {toggleChecked(product.id, product.checked)}}>
-                    <label class="align-middle" for="checkbox+{product.id}" on:click={() => {toggleChecked(product.id, product.checked)}}>{product.title}</label>
-                </div>
-                <input class="col-span-1 m-auto mt-0.5" type="image" src="/delete.png" alt="delete" on:click={() => {deleteProduct(product.id)}}> -->
-            </div>
-        {/each}
+<main class="bg-marine md:px-40 lg:px-64 xl:px-96">
+    <div class="text-center text-bee pb-1">
+        <h1 class="text-4xl py-5 font-semibold">Einkaufsliste</h1>
+        <form class="flex mb-4 mt-1 justify-center" on:submit|preventDefault={handleInput}>
+            <input class="m-0 w-3/4 h-8 px-2 bg-bee border-none text-lg text-marine font-semibold rounded-xl" type="text" bind:value={input}>
+            <button class="text-lg font-semibold px-2 ml-1.5 bg-bee text-marine rounded-xl" type="submit">Add</button>
+        </form>
+        <div class="products bg-marine">
+            {#each products as product}
+                {#if !product.checked}
+                    <div class="Card text-2xl text-left p-1.5 m-2.5 bg-marine-bright rounded-xl break-words flex items-center">
+                        <img class="h-10 w-10 mr-1.5 text-xs" type="image" src="/category/{product.category}.svg" alt={product.category}>
+                        <p class="" on:click={() => {toggleChecked(product.id, product.checked)}}>{product.title}</p>
+                        <input class="ml-auto mr-1 w-6 h-6" type="checkbox" checked={product.checked} on:click={() => {toggleChecked(product.id, product.created, product.checked)}}>
+                        <input class="mt-0.5 h-7 w-7 text-xs" type="image" src="/delete.svg" alt="delete" on:click={() => {deleteProduct(product.id)}}>
+                    </div>
+                {/if}
+            {/each}
+        </div>
+        <div class="checkedProducts mt-10">
+            {#each products as product}
+                {#if product.checked}
+                    <div class="checkedCard text-2xl text-left p-1.5 m-2.5 bg-marine-bright rounded-xl break-words flex items-center">
+                        <input class="h-10 w-10 mr-1.5" type="image" src="/category/{product.category}.svg" alt="(delete)">
+                        <p class="" on:click={() => {toggleChecked(product.id, product.created, product.checked)}}>{product.title}</p>
+                        <input class="ml-auto mr-1 w-6 h-6" type="checkbox" checked={product.checked} on:click={() => {toggleChecked(product.id, product.created, product.checked)}}>
+                        <input class="mt-0.5 h-7 w-7 text-xs" type="image" src="/delete.svg" alt="delete" on:click={() => {deleteProduct(product.id)}}>
+                    </div>
+                {/if}
+            {/each}
+        </div>
     </div>
 </main>
