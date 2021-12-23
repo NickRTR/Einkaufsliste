2556dd3a2f934ed071495c3d49a2633d8f891aeb
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-23 16:35:24 +0100

    update

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,41 +2,41 @@
 
 export const categories = {
     // Milch
-    milk: ["milch", "butter", "sahne", "joghurt", "käse", "frischkäse", "grießbrei"],
+    milk: ["milch", "butter", "sahne", "joghurt", "käse", "frischkäse", "grießbrei", "milchpulver", "reisbrei", "schmand", "schlagsahne", "saure sahne", "milchstreichfett", "milchpulver", "butterschmalz", "käse", "quark"],
 
     // Wurst, Fleisch
-    sausage: ["fleisch", "wurst", "schinken"],
+    sausage: ["fleisch", "wurst", "schinken", "lyoner", "salami", "pizzafleischkäse", "bierschinken", "currywurst", "bratwurst", "leberwurst", "landjäger", "weißwurst", "saitenwurst"],
 
     // Gefrorenes
-    frozen: ["pommes", "gefroren"],
+    frozen: ["pommes", "kroketten", "hähnchen", "eis", "gefrorene himbeeren", "frosta", "pommes", "fisch"],
 
     // Früchte, Obst
-    fruit: ["obst", "früchte", "apfel", "äpfel", "kirsche", "melone"],
+    fruit: ["obst", "früchte", "apfel", "äpfel", "kirschen", "melone", "birnen", "bananen", "orangen", "aprikosen", "brombeeren", "erdbeeren", "himbeeren", "johannisbeeren", "heidelbeeren", "kiwi", "avocado", "ananas", "pfirsich", "trauben", "rosinen", "granatapfel", "granatäpfel", "zitronen", "limetten", "mango", "aprikosen", "litschi", "kokosnuss", "mandarine"],
 
     // Gemüse
-    vegetables: ["paprika", "zwiebel", "kartoffel", "gurke", "salat"],
+    vegetables: ["paprika", "zwiebel", "kartoffel", "gurke", "salat", "karotten", "erbsen", "bohnen", "brokkoli", "blumenkohl", "avocado", "fenchel", "kohl", "gurke", "möhren", "knoblauch", "kichererbsen", "pilze", "kohlrabi", "oliven", "kürbis", "auberginen", "radieschen", "rhabarber", "rosenkohl", "sellerie", "spinat", "spargel", "tomaten", "zucchini", "lauch", "zwiebeln", "spargel", "kürbis"],
 
     // Dosenartikel
     canned: ["ravioli", "dosenwurst", "bohnen"],
 
     // Getränke
-    drink: ["bier", "wasser", "saft", "orangensaft", "alkohol"],
+    drink: ["bier", "wasser", "saft", "orangensaft", "alkohol", "kinderpunsch", "glühwein", "sprudel", "spezi", "fanta", "cola", "capri sonne", "energy drink", "traubensaft", "redbull", "durstlöscher", "sprite"],
 
     // Drogierieartikel
-    drogery: ["waschmittel", "spülmittel", "zahnpasta", "duschgel"],
+    drogery: ["waschmittel", "spülmittel", "zahnpasta", "duschgel", "zahnbürste", "handseife", "seife", "lappen", "deo", "gel", "wachs", "rasierer", "kamm", "duschseife", "handtuch", "parfüm"],
 
     // Nudeln
-    noodles: ["nudeln", "spaghetti"],
+    noodles: ["nudeln", "spaghetti", "maccheroni", "fusilli", "penne", "rigatoni"],
 
     // Süßigkeiten
-    sweets: ["nimm2", "haribo", "gummibärchen", "schokolade"],
+    sweets: ["nimm2", "haribo", "gummibärchen", "schokolade", "twixx","smarties", "marsriegel", "chips", "m&m's", "snickers", "toblerone", "donuts", "kinder schokolade", "giotto", "lolli", "duplo", "oreo", "trolliwürmer", "kaugummi", "ritter sport schokolade", "ahoi brause", "esspapier"],
 
     // Brot, Gebäck
-    bread: ["brot", "brötchen", "wecken", "gebäck"],
+    bread: ["brot", "brötchen", "wecken", "gebäck", "laugenstangen", "toast", "brezel"],
 
     // Soßen
-    sauce: ["soße", "bratensoße", "ketchup"],
+    sauce: ["soße", "bratensoße", "ketchup", "rindersoße", "rahmsoße", "bechamelsoße", "hollandaise", "tomatensoße", "mayonnaise"],
 
     // Brotaufstrich
-    jam: ["marmelade", "erdbeermarmelade", "kirschmarmelade", "nutella"],
+    jam: ["marmelade", "erdbeermarmelade", "kirschmarmelade", "nutella", "honig"],
 }
