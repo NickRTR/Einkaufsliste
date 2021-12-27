4dddeba03b9650f1b05113d390a34f0a9891b66d
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-27 19:52:18 +0100

    added new List button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -49,7 +49,10 @@
 <main class="bg-marine min-h-screen text-center md:px-40 lg:px-64 xl:px-96">
     <div class="text-bee pb-1">
         <h1 class="text-4xl pt-5 font-semibold">Einkaufsliste</h1>
-        <button class="text-white mt-2 mr-4 underline" on:click={toggleLogin}>Liste verbinden</button>
+        <div>
+            <button class="text-white mt-2 underline" on:click={toggleLogin}>Liste verbinden</button><br>
+            <button class="text-white mt-2 underline" on:click={() => {localStorage.clear(); location.reload()}}>Neue Liste</button>
+        </div>
         <div class="{showConnect} bg-marine-bright rounded-xl m-5"><ConnectScreen></ConnectScreen></div>
         <form class="flex mt-4 mb-2 justify-center" on:submit|preventDefault={() => {addProduct(input)}}>
             <input class="m-0 w-3/4 h-8 px-2 bg-bee border-none text-lg text-marine font-semibold rounded-xl" type="text" bind:value={input}>
