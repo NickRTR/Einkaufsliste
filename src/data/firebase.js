fc343fb392883ae3cf9f344c1a27ce37ced807c6
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-28 12:26:30 +0100

    500 error try fix

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -5,16 +5,23 @@ import {categories} from "../data/categories.js";
 import products from "./store.js";
 
 // initialize Database
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyAfARrvqE1zBN-MQIsornrEBcZTiw1LsM4",
-    authDomain: "einkaufsliste-svelte.firebaseapp.com",
-    projectId: "einkaufsliste-svelte",
-    storageBucket: "einkaufsliste-svelte.appspot.com",
-    messagingSenderId: "117071138476",
-    appId: "1:117071138476:web:3a3b2ccef76b53aa545909"
-});
-
-const db = getFirestore(); 
+while (true) {
+    try {
+        const firebaseApp = initializeApp({
+            apiKey: "AIzaSyAfARrvqE1zBN-MQIsornrEBcZTiw1LsM4",
+            authDomain: "einkaufsliste-svelte.firebaseapp.com",
+            projectId: "einkaufsliste-svelte",
+            storageBucket: "einkaufsliste-svelte.appspot.com",
+            messagingSenderId: "117071138476",
+            appId: "1:117071138476:web:3a3b2ccef76b53aa545909"
+        });
+        
+        var db = getFirestore(); 
+        break;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const createCollection = async (listName) => {
     const share = await getDoc(doc(db, listName, "share"));
