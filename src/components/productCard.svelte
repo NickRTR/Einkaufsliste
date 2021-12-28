438b801d52ebc5964610fc79bb95665f484645f1
NickRTR <95316598+NickRTR@users.noreply.github.com>
2021-12-28 15:14:32 +0100

    fixed line break

--- src/components/productCard.svelte
+++ src/components/productCard.svelte
@@ -16,7 +16,7 @@
 </script>
  
 <div class="container mx-auto">
-    <div class="Card text-2xl p-1.5 m-2.5 bg-marine-bright rounded-xl break-words flex items-center">
+    <div class="Card text-2xl p-1.5 m-2.5 bg-marine-bright rounded-xl flex items-center break-words">
         <img class="h-10 w-10 mr-1.5 text-xs" type="image" src="/category/{product.category}.svg" alt={product.category} title={product.category} on:click={toggleCategoryScreen}>
         <p on:click={() => {toggleChecked(product.id, product.checked)}}>{product.title}</p>
         <input class="ml-auto mr-1 w-6 h-6" type="checkbox" checked={product.checked} on:click={() => {toggleChecked(product.id, product.created, product.checked)}}>
