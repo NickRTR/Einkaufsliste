25e5bf19ece26b0443c4a74a5b81a9ba3fad7c8a
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-02-21 08:31:10 +0100

    Translated to Chinese

--- src/lib/translations/translate.js
+++ src/lib/translations/translate.js
@@ -1,8 +1,6 @@
 export const translate = async (language) => {
     let languageTranslation;
 
-    console.log(language);
-    
     if (language === "en-US") {
         languageTranslation = await import("$lib/translations/en_us");
         return languageTranslation.translation;
@@ -11,6 +9,9 @@ export const translate = async (language) => {
     switch (language.substr(0,2)) {
         case "es":
             languageTranslation = await import("$lib/translations/es");
+            return languageTranslation.translation;
+        case "zh":
+            languageTranslation = await import("$lib/translations/zh");
             return languageTranslation.translation;
         case "fr":
             languageTranslation = await import("$lib/translations/fr");
---
+++ src/lib/translations/zh.js
@@ -0,0 +1,35 @@
+export const translation = {
+    index: {
+        welcome: "歡迎",
+        logout: "登出",
+        sort: "排序類別",
+        add: "添加",
+        pcs: "片",
+        gr: "克",
+        kg: "公斤",
+        ml: "毫升",
+        l: "升"
+    },
+    categories: {
+        Gemüse: "蔬菜",
+        Obst: "蔬菜",
+        Vorrat: "蔬菜",
+        Fleisch: "蔬菜",
+        Gefriertruhe: "冷凍",
+        Kühlregal: "冷藏",
+        Haushalt: "冷藏",
+        Süßigkeiten: "糖果",
+        Getränke: "飲料"
+    },
+    login: {
+        registered: {
+            title: "登錄",
+            switch: "建立新帳戶?"
+        },
+        unregistered: {
+            title: "註冊",
+            switch: "您是否已經創建了一個帳戶?"
+        },
+        password: "密碼"
+    }
+}
