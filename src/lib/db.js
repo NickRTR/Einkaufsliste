4b7899c90da1fbe111855660436b758b1cafbaef
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-24 15:04:59 +0100

    Switch to dotenv for environment variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-let URL = import.meta.env.VITE_SUPABASE_URL;
-let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-// if production, get KEY from Vercel
-if (process.env.NODE_ENV === 'production') {
-    URL = process.env.URL;
-    KEY = process.env.KEY;
-}
+dotenv.config();
+
+const URL = process.env.SUPABASE_URL;
+const KEY = process.env.SUPABASE_KEY;
 
 const supabase = createClient(
   URL,
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-            define: {
-                'process.env': process.env,
-            },
-        },
+		adapter: adapter()
 	}
 };
 
