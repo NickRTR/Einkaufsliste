013f60027fa4acee7a74237872df0374b428eaed
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-10 17:05:27 +0200

    update env

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,7 +5,7 @@ let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
 if (process.env.NODE_ENV === 'production') {
     URL = process.env.SUPABASE_URL;
-    KEY = process.env.SUPABASE_KEY;
+    KEY = process.env.SUPABASE_ANON_KEY;
 }
 
 const supabase = createClient(
