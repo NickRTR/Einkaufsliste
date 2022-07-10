f06365ff1c360eb801501a9e8bd16d42639dffc4
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-10 16:41:00 +0200

    updated env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-let URL = import.meta.env.VITE_SUPABASE_URL;
-let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-// if production, get KEY from Vercel
-if (process.env.NODE_ENV === 'production') {
-    URL = process.env.URL;
-    KEY = process.env.KEY;
-}
+dotenv.config();
 
 const supabase = createClient(
-  URL,
-  KEY
+  process.env["SUPABASE_URL"],
+  process.env["SUPABASE_ANON_KEY"]
 )
 
-export default supabase
+export default supabase;
