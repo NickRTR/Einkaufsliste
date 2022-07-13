585a99c066dd9eff1ef4056975021cfdb8cf86cc
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:55:00 +0200

    s

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,9 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.MODE === 'production' || process.env.MODE === 'production') {
     URL = process.env.SUPABASE_URL;
     KEY = process.env.SUPABASE_ANON_KEY;
+    production();
 }
 
 const supabase = createClient(URL, KEY);
