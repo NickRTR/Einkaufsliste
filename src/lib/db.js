778176e0e8ab53d6f09a8771a0f0e5f3cf54a4fc
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:12:34 +0200

    add dotenv

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from "dotenv";
 
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
 if (process.env.NODE_ENV === 'production') {
+    dotenv.config()
+
     URL = process.env.SUPABASE_URL;
     KEY = process.env.SUPABASE_ANON_KEY;
 }
