29ca600a5f887e0d16b4d614b6fd2c83b0ee05da
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:10:51 +0200

    Check for env

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,23 +1,13 @@
-// import { createClient } from '@supabase/supabase-js';
-
-// console.log("db is called");
-
-// async function createSupabaseClient() {
-//     const res = await fetch("/api/getSession");
-//     const data = await res.json();
-//     return createClient(data.URL, data.KEY);
-// }
-
-// export default createSupabaseClient();
-
 import { createClient } from '@supabase/supabase-js';
 
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// if production, get KEY from Vercel
+if (process.env.NODE_ENV === 'production') {
+    URL = process.env.SUPABASE_URL;
+    KEY = process.env.SUPABASE_ANON_KEY;
+}
 
-const supabase = createClient(
-  URL,
-  KEY
-)
+const supabase = createClient(URL, KEY);
 
 export default supabase;
