bd9d379cdd9e3e245951aed113cef9bce7630612
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 16:06:27 +0200

    !

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,10 +4,7 @@ import { browser } from '$app/env';
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
-if (import.meta.env.MODE === 'production' || process.env.MODE === 'production' && browser) {
-    console.table("Vite:" + import.meta.env);
-    console.log("Vercel:" + process.env);
-
+if (import.meta.env.MODE === 'production' || process.env.MODE === 'production' && !browser) {
     URL = process.env.SUPABASE_URL;
     KEY = process.env.SUPABASE_ANON_KEY;
 }
