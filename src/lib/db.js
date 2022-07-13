4d26ad4d50ab86fdef1477e25df68009e95243b1
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:19:57 +0200

    use endpoint

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,18 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from "dotenv";
 
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
 if (import.meta.env.MODE === 'production') {
-    console.log("production mode");
+    const res = await fetch("/api/getEnv");
+    const data = await res.json();
 
-    dotenv.config();
-
-    console.log(process.env);
-
-    URL = process.env["SUPABASE_URL"];
-    KEY = process.env["SUPABASE_ANON_KEY"];
+    URL = data.URL
+    KEY = data.KEY
 }
 
 const supabase = createClient(URL, KEY);
