c2ed9272d028c43812f27af24b982cda590cd879
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:22:33 +0200

    remove top level await

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
-let URL = import.meta.env.VITE_SUPABASE_URL;
-let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-// if production, get KEY from Vercel
-if (import.meta.env.MODE === 'production') {
+function production() {
     const res = await fetch("/api/getEnv");
     const data = await res.json();
 
-    URL = data.URL
-    KEY = data.KEY
+    URL = data.URL;
+    KEY = data.KEY;
 }
+
+let URL = import.meta.env.VITE_SUPABASE_URL;
+let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// if production, get KEY from Vercel
+if (import.meta.env.MODE === 'production') production();
 
 const supabase = createClient(URL, KEY);
 
