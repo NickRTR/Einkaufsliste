2a2d142beaccd867fe068d062817c340cb3808e1
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 16:04:07 +0200

    browser

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import { browser } from '$app/env';
 
 let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
-if (import.meta.env.MODE === 'production' || process.env.MODE === 'production') {
-    console.log("Vite:" + import.meta.env);
+if (import.meta.env.MODE === 'production' || process.env.MODE === 'production' && browser) {
+    console.table("Vite:" + import.meta.env);
     console.log("Vercel:" + process.env);
 
     URL = process.env.SUPABASE_URL;
