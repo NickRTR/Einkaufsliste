6c24f456af95d07a34d28128196f7e16189fff84
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 16:00:10 +0200

    add debugging statements

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,6 +4,9 @@ let URL = import.meta.env.VITE_SUPABASE_URL;
 let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // if production, get KEY from Vercel
 if (import.meta.env.MODE === 'production' || process.env.MODE === 'production') {
+    console.log("Vite:" + import.meta.env);
+    console.log("Vercel:" + process.env);
+
     URL = process.env.SUPABASE_URL;
     KEY = process.env.SUPABASE_ANON_KEY;
 }
