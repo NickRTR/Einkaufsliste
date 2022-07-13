b7e119f6b1552a423b15b0d33c5fbec279de0209
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 14:57:40 +0200

    removed top-level await

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 
 console.log("db is called");
 
-const res = await fetch("/api/getSession");
-const data = await res.json();
+async function createSupabaseClient() {
+    const res = await fetch("/api/getSession");
+    const data = await res.json();
+    return createClient(data.URL, data.KEY);
+}
 
-const supabase = createClient(data.URL, data.KEY);
-
-export default supabase;
+export default createSupabaseClient();
