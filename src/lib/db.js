1a5e8d857f1b13b2e730f38d2738b1dee8b16056
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-13 15:01:09 +0200

    trying different approach

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,23 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// console.log("db is called");
+
+// async function createSupabaseClient() {
+//     const res = await fetch("/api/getSession");
+//     const data = await res.json();
+//     return createClient(data.URL, data.KEY);
+// }
+
+// export default createSupabaseClient();
+
 import { createClient } from '@supabase/supabase-js';
 
-console.log("db is called");
+let URL = import.meta.env.VITE_SUPABASE_URL;
+let KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-async function createSupabaseClient() {
-    const res = await fetch("/api/getSession");
-    const data = await res.json();
-    return createClient(data.URL, data.KEY);
-}
+const supabase = createClient(
+  URL,
+  KEY
+)
 
-export default createSupabaseClient();
+export default supabase;
