9753762d5fa669a9b4698c08acc97ab762c55b6e
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-18 07:40:12 +0200

    getProducts error logging

--- src/routes/api/getProducts.js
+++ src/routes/api/getProducts.js
@@ -4,6 +4,7 @@ export async function get() {
     let { data: products, error } = await supabase.from("products").select('*');
 
     if (error) {
+        console.error(error.message);
         return {
             status: error.status,
             body: {
