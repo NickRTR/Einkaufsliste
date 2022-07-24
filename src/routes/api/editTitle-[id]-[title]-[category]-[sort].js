1108e3e475641ac970663235565e8c1b5b91330f
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-24 17:55:10 +0200

    change sort on edit title

--- src/routes/api/editTitle-[id]-[title]-[category]-[sort].js
+++ src/routes/api/editTitle-[id]-[title]-[category]-[sort].js
@@ -1,9 +1,9 @@
 import supabase from "$lib/supabase";
 
 export async function GET({ params }) {
-  let { id, title, category } = params;
+  let { id, title, category, sort } = params;
 
-  const { error } = await supabase.from("products").update({ title, category }).eq("id", id);
+  const { error } = await supabase.from("products").update({ title, category, sort }).eq("id", id);
 
   if (error) {
     console.error(error.message);
