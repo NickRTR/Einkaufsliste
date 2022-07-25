5cd281a62ed4113a373e9cf67ab6b1f8ec7ed4be
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-25 21:57:52 +0200

    fixed bug

--- src/routes/api/updateCategories.js
+++ src/routes/api/updateCategories.js
@@ -1,6 +1,6 @@
 import supabase from "$lib/supabase";
 
-export async function POST({ locals }) {
+export async function POST({ request, locals }) {
 	const { categories } = await request.json();
 
 	const { error } = await supabase.from("userdata").update({ categories }).eq("uuid", locals.user.id);
