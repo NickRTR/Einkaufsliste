240827175ee6aad047c98f560e321285d35fc85a
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-13 22:02:22 +0200

    fix addProduct api endpoint

--- src/routes/api/product/addProduct-[title]-[category]-[sort]/+server.js
+++ src/routes/api/product/addProduct-[title]-[category]-[sort]/+server.js
@@ -3,7 +3,7 @@ import supabase from "$lib/supabase";
 export async function GET({ params }) {
 	const { title, category, sort } = params;
 
-	const { data, error } = await supabase.from("products").insert([{ title, uuid: supabase.auth.user().id, category, sort }]);
+	const { data, error } = await supabase.from("products").insert([{ title, category, sort }]);
 
 	if (error) {
 		return new Response(JSON.stringify({ error: error.message }));
