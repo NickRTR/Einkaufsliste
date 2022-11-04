39720b09db96abe58f0717c0a8d5075d17afa551
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-04 22:23:36 +0100

    npm run format

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: 'src/',
-	globPatterns: [
-		'**/*.{html,js,svelte}'
-	],
-	swDest: 'src/service-worker.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	globDirectory: "src/",
+	globPatterns: ["**/*.{html,js,svelte}"],
+	swDest: "src/service-worker.js",
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/]
 };
