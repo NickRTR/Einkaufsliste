0b95ea6b870f9b85bada2ba322decbd69bd4b419
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-09 16:49:38 +0100

    complete feedback form

--- src/lib/components/Feedback.svelte
+++ src/lib/components/Feedback.svelte
@@ -1,8 +1,13 @@
 <script>
 	import { wordList } from "$lib/stores";
-	import { page } from "$app/stores";
 
-	let submitted = $page.url.search === "?thanks=true";
+	let submitted = false;
+
+	async function toggleSubmitted() {
+		// wait for 1 second in order to let the form carry out
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		submitted = true;
+	}
 </script>
 
 <svelte:head>
@@ -13,14 +18,18 @@
 	{#if submitted}
 		<p class="thanks">{$wordList.settings.feedback.thanks}</p>
 	{:else}
-		<form action="https://formsubmit.co/nickrtrrtr@gmail.com" method="POST">
+		<form id="form" action="https://formsubmit.co/nickrtrrtr@gmail.com" target="_blank" method="POST">
 			<label for="email">{$wordList.settings.feedback.nameLabel}</label>
-			<input type="text" name="name" id="name" placeholder={$wordList.settings.feedback.name} required />
+			<input type="text" name="name" id="name" placeholder={$wordList.settings.feedback.name} />
 			<label for="message">{$wordList.settings.feedback.messageLabel}</label>
 			<textarea name="message" id="message" placeholder={$wordList.settings.feedback.message} required />
-			<button type="submit">{$wordList.settings.feedback.submit}</button>
+			<button
+				on:click={() => {
+					toggleSubmitted();
+				}}
+				type="submit">{$wordList.settings.feedback.submit}</button
+			>
 			<input type="hidden" name="_subject" value="Schoppy: New feedback submission!" />
-			<input type="hidden" name="_next" value="https://schoppy.vercel.app/settings?thanks=true" />
 		</form>
 	{/if}
 </main>
