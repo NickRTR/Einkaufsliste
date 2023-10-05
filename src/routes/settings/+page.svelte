29344a4cba90f4948ab5fc659af5bae1867a0baf
Nick <95316598+NickRTR@users.noreply.github.com>
2023-10-05 18:56:42 +0200

    Add settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { _ } from "svelte-i18n";
+</script>
+
+<svelte:head>
+	<title>{$_("pages.settings.title")}</title>
+</svelte:head>
+
+<body>
+	<h1>{$_("pages.settings.title")}</h1>
+</body>
